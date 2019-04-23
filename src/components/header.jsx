baea4476bf2c215e7d4eb70730a6c907b4f05b9b
James DeLay <jdelay.jr@gmail.com>
2019-04-22 21:30:16 -0400

    Responsive Design
    Adding flexible box for responsive design

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "../styles/header.css";
 
 const Header = () => (
-  <header>
-    <div className="header-container">
-      <h1 className="header-main">Eric Lizzul</h1>
+  <header className="col-12">
+    <div className="col-12 header-container">
+      <h1 className="col-12 header-main">Eric Lizzul</h1>
       <h2 className="header-sub">Video Producer & Content Creator</h2>
     </div>
   </header>

b89335733709a90e94a9bceaa693b08c9b02ad04
James DeLay <jdelay.jr@gmail.com>
2019-04-22 22:05:53 -0400

    Update bio.jsx
    removing old info

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -6,19 +6,7 @@ import "../styles/bio.css";
 const Bio = () => (
   <div className="bio-container">
     <div className="col-4">left</div>
-    <div className="col-8">
-      <p>
-        I graduated from Providence College in 2016 with a degree in marketing
-        and I am currently attending Hofstra University in pursuit of my masters
-        degree in Journalism.
-      </p>
-      <p>
-        {" "}
-        I hosted a radio show on campus with my friends called “Nose Bleed
-        Sports” where I primarily focused on hockey, but covered other sports
-        such as baseball, football, basketball and golf.
-      </p>
-    </div>
+    <div className="col-8">right</div>
   </div>
 );
 

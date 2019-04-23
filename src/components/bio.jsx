de7bd9cd68c140bc37115a0694d163c992b4af4b
James DeLay <jdelay.jr@gmail.com>
2019-04-22 22:02:09 -0400

    Name Change
    changing name

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -1,10 +1,24 @@
+/* eslint-disable react/jsx-one-expression-per-line */
 import React from "react";
+// eslint-disable
 import "../styles/bio.css";
 
 const Bio = () => (
   <div className="bio-container">
-    <div className="bio-left">left</div>
-    <div className="bio-right">right</div>
+    <div className="col-4">left</div>
+    <div className="col-8">
+      <p>
+        I graduated from Providence College in 2016 with a degree in marketing
+        and I am currently attending Hofstra University in pursuit of my masters
+        degree in Journalism.
+      </p>
+      <p>
+        {" "}
+        I hosted a radio show on campus with my friends called “Nose Bleed
+        Sports” where I primarily focused on hockey, but covered other sports
+        such as baseball, football, basketball and golf.
+      </p>
+    </div>
   </div>
 );
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-closing-tag-location */
+/* eslint-disable react/jsx-indent */
 import React from "react";
 import "../styles/header.css";
 
 const Header = () => (
-  <header className="col-12">
-    <div className="col-12 header-container">
-      <h1 className="col-12 header-main">Eric Lizzul</h1>
-      <h2 className="header-sub">Video Producer & Content Creator</h2>
-    </div>
-  </header>
+    <header>
+        <div className="header-container">
+            <h1 className="col-12 header-main">James DeLay</h1>
+            <h2 className="col-12 header-sub">Full-stack JavaScript Developer</h2>
+        </div>
+    </header>
 );
 
 export default Header;

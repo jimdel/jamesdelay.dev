5d9232f1825dbecaec8a8c6b2516772e1e032b91
James DeLay <jdelay.jr@gmail.com>
2019-04-25 21:27:36 -0400

    outline for bio
    outline for bio

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -3,11 +3,16 @@ import "../styles/bio.css";
 
 const Bio = () => (
   <div className="row bio-container">
-    <div className="col-xs-4">
+    <div className="col-lg-4 col-xs-12">
       <div className="box">left</div>
     </div>
-    <div className="col-xs-8">
-      <div className="box">right</div>
+    <div className="col-lg-8 col-xs-12">
+      <div className="box">
+        <p>Paragraph about me</p>
+        <p>Paragraph about education/background</p>
+        <p>Paragraph about how I started coding</p>
+        <p>Paragraph about what I am passionate about (code related)</p>
+      </div>
     </div>
   </div>
 );

9848d61dc1f9ea1eab61066d5436fdaceafc1b34
James DeLay <jdelay.jr@gmail.com>
2019-07-31 23:20:51 -0400

    fixing header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import "../styles/header.css";
 
 const Header = () => (
-    <header>
-        <div className="row start-xs header-container">
-            <h1 className="col-lg-12 header-main">James DeLay</h1>
-            <h2 className="col-lg-12 header-sub">Full-stack JavaScript Developer</h2>
-        </div>
-    </header>
+  <header>
+    <div className="row start-xs header-container">
+      <h1 className="col-lg-12 header-main">James DeLay</h1>
+      <h2 className="col-lg-12 header-sub">JavaScript Developer</h2>
+    </div>
+  </header>
 );
 
 export default Header;

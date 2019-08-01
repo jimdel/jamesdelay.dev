bb431adfcefb5c496b85f1c5a96271a58375f95d
James DeLay <jdelay.jr@gmail.com>
2019-07-31 23:16:19 -0400

    working on projects section

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import "../styles/projects.css";
 
 const Projects = () => (
-  <div className="projects-container">
-    <p>Projects</p>
-  </div>
+  <section className="projects-container projects-grid-container">
+    <div className="projects-grid-cell">
+      <p>1</p>
+    </div>
+  </section>
 );
 
 export default Projects;

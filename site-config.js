0124850f75a73708f9e130997a4888906e087db5
JDeLay-JR <jdelay.jr@gmail.com>
2019-10-01 20:46:18 -0400

    feat: change link

--- site-config.js
+++ site-config.js
@@ -4,7 +4,7 @@ module.exports = {
   linkedIn: 'https://www.linkedin.com/in/jdelayjr/',
   codepen: "https://codepen.io/jdelay-jr",
   dribbble: "https://dribbble.com/JDeLay-JR",
-  tinkleCode: 'https://github.com/akramer24/tinkle',
+  tinkleCode: 'https://github.com/JDeLay-JR/tinkle',
   blogCode: 'https://github.com/JDeLay-JR/BlogSite_2.0',
   blogLink: "https://itsatechblog.netlify.com",
   scrapprCode: 'https://github.com/ScrapprApp/Scrapbooker',
